d313c4d4fea1156d172709798ce18aa47291fd5d
alphasec <95212748+alphasecio@users.noreply.github.com>
2024-07-03 16:11:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
+import requests
 import pandas as pd
 import streamlit as st
-from forex_python.converter import CurrencyRates
 
-# Initialize the CurrencyRates object
-c = CurrencyRates()
-
-# Set the page title
+# Set the page title and get ExchangeRate API key
 st.set_page_config(page_title="Currency Converter")
 st.subheader("Currency Converter")
+with st.sidebar:
+    exchangerate_api_key = st.text_input("ExchangeRate API key", type="password")
 
 # Get the base currency, quote currency and amount from the user
 col1, col2 = st.columns(2)
@@ -24,16 +23,30 @@ quote_currency = st.multiselect(
 
 # Convert the base amount to the selected currencies
 if st.button("Convert"):
-    try:
-        with st.spinner("Please wait..."):
-            data = []
-            data.append(["Value", "Currency"])
-            for currency in quote_currency:
-                rate = c.get_rate(base_currency, currency)
-                converted_amount = base_amount * rate
-                data.append([f"{converted_amount:.2f}", currency])
-            
-            df = pd.DataFrame(data[1:], columns=data[0])
-            st.dataframe(df, width=200, hide_index=True)
-    except Exception as e:
-        st.exception(f"Exception: {e}")
+    if not exchangerate_api_key.strip():
+        st.error("Please provide a valid API key.")
+    else:
+        try:
+            with st.spinner("Please wait..."):
+                data = []
+                data.append(["Value", "Currency"])
+                for currency in quote_currency:
+                    url = f"https://v6.exchangerate-api.com/v6/{exchangerate_api_key}/latest/{base_currency}"
+                    response = requests.get(url)
+                    quote = response.json()
+
+                    # Ensure the API call was successful and the currency is in the response
+                    if response.status_code == 200 and "conversion_rates" in quote:
+                        rate = quote["conversion_rates"].get(currency)
+                        if rate:
+                            converted_amount = base_amount * rate
+                            data.append([f"{converted_amount:.2f}", currency])
+                        else:
+                            st.error(f"Currency {currency} not found in conversion rates.")
+                    else:
+                        st.error(f"Error fetching data: {quote.get('error-type', 'Unknown error')}")
+                
+                df = pd.DataFrame(data[1:], columns=data[0])
+                st.dataframe(df, width=200, hide_index=True)
+        except Exception as e:
+            st.exception(f"Exception: {e}")
